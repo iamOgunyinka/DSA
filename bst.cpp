aec8a5490399708c0ee99081ac1ee9e5c23acb06
Aswini S <ashumeow@live.com>
2015-01-11 01:05:04 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -131,6 +131,10 @@ namespace dsa
             /*
             breadth_first_traversal( n->visited );
             std::cout << n->value << " ";
+            if (n->Left() != NULL)
+			q.push(*n->Left());
+		    if (n->Right() != NULL)
+			q.push(*n->Right());
             */
         }
         
